a4da4c90a8e040799078041bb1e52752747b663c
tuan5 <tuan051002@gmail.com>
2022-07-23 17:00:22 +0700

    Commit#2

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,18 @@ var fs = require('fs')
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  fs.readFile('data/data.txt',function (err,data){
-    if (err != null){
-      res.send(err.message)
-    }
-    else {
-      res.send(data.toString())
-    }
-  })
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+})
+
+router.get('/getUsers', function (req, res, next) {
+    fs.readFile('data/data.txt', function (err, data) {
+        if (err != null) {
+            res.send(err.message)
+        } else {
+            res.send(data.toString())
+        }
+    })
 });
 
 module.exports = router;

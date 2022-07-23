844e86a9a5045a4f24d4eeca568c1f8b23dc225a
tuan5 <tuan051002@gmail.com>
2022-07-23 17:54:41 +0700

    Commit#4

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const fs = require('fs');
 const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
 const router = express.Router();
+const storage = multer.diskStorage({
+    destination: function (req,file,cb){
+        cb(null,'uploads')
+    },
+    filename: function (req,file,cb){
+        cb(null,Date.now()+'-'+file.originalname)
+    }
+})
 
+const upload = multer({ storage: storage })
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', {title: 'Express'});
